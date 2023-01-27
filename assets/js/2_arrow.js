24c5fff10d9b46250dbb39ba225afe2ea95f57f0
Felipe Javier Chirino Sepúlveda <felipe.chirino@gmail.com   >
2023-01-27 10:57:57 -0300

    main

---
+++ assets/js/2_arrow.js
@@ -0,0 +1,20 @@
+
+function suma2 (a, b) {
+    return a + b;
+}
+
+let suma = function(a, b){
+    return a + b;
+} 
+
+let sumar= (a,b) => {
+    return a+b;
+}
+
+let sumar2 = (a,b) => a + b;
+
+
+let prueba = () =>{
+}
+
+
